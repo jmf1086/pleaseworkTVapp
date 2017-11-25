6ad2d13d474570f8a0c9945fa0c2e55ab14a2d68
jmf1086 <jmf1086@yahoo.com>
2017-11-25 16:33:22 -0600

    pleasewrk
    wrk

--- JoeyTvApp/www/login.php
+++ JoeyTvApp/www/login.php
@@ -1,4 +1,31 @@
 <!doctype html>
+<?php
+
+$servername = "joeytvserver.database.windows.net";
+$username="jmf1086";
+$password="homefry1";
+$dbname="TvShows";
+//create connection
+$conn=new MySQLi($servername,$username,$password,$dbname);
+
+//check the connection 
+if($conn->connect_error){
+	die("connection failed".$conn->connect_error);
+}
+	if(isset($_POST['user'])){
+		$user=$_POST['user'];
+		$pass =$_POST['pass'];
+		$sql ="select * from login where username = '".$user."' and password='".$pass."' LIMIT 1";
+		$result = $conn->query($sql);
+		if($result->num_rows>0){
+			echo("you have successfully loggined");
+			exit();
+		}else{
+			echo("goto previous page");
+			exit();
+		}
+	}
+?>
 <html>
 <head>
 <meta charset="utf-8">
@@ -7,7 +34,8 @@
 
 <body>
 <h1>Login</h1>
-<form name="login_form">
+<form name="login_form"
+method="post" action= index.php>
 <label>Username:<br/></label>
 <input name="username" type="text" autofocus="autofocus" required="required"><br/>
 <label>Password:<br/></label>
