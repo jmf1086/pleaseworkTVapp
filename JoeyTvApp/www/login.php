e09ffec060a3a80878ad7e9f799e7e4f9ec30b69
jmf1086 <jmf1086@yahoo.com>
2017-11-25 17:40:43 -0600

    1
    1

--- JoeyTvApp/www/login.php
+++ JoeyTvApp/www/login.php
@@ -35,12 +35,12 @@ if($conn->connect_error){
 <body>
 <h1>Login</h1>
 <form name="login_form"
-method="post" action= index.php>
+>
 <label>Username:<br/></label>
 <input name="username" type="text" autofocus="autofocus" required="required"><br/>
 <label>Password:<br/></label>
 <input name="pasword" type="password" autofocus="autofocus" required="required"><br/>
-<input type="submit" value=login>
+<input type="submit" formaction="index.php" value=login>
 </form>
 </body>
 </html>
