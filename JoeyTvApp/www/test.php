73a6a8e7ee04093ea80d17771b02093b851f86db
jmf1086 <jmf1086@yahoo.com>
2017-11-27 19:17:05 -0600

    b
    fhf

--- JoeyTvApp/www/test.php
+++ JoeyTvApp/www/test.php
@@ -2,14 +2,14 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:joeytvserver.database.windows.net,1433; Database = TvShows", "jmf1086", "homefry1");
+    $conn = new PDO("sqlsrv:server = tcp:joeytvserver.database.windows.net,1433; Database = TvShows", "jmf1086", "Homefry1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-?>
+
 <html>
 <head>
 <meta charset="utf-8">
