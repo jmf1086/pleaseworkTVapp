2fb59f95636802f443326f572b9cc316f25555cb
jmf1086 <jmf1086@yahoo.com>
2017-11-27 19:09:15 -0600

    t
    h

---
+++ JoeyTvApp/www/test.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:joeytvserver.database.windows.net,1433; Database = TvShows", "jmf1086", "homefry1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
