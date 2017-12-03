fe96d24d7843d3d63ae43b321fa3bdebe74a73ac
jmf1086 <jmf1086@yahoo.com>
2017-12-03 06:08:15 -0600

    d
    d

--- JoeyTvApp/JoeyTvApp/www/config.php
+++ JoeyTvApp/JoeyTvApp/www/config.php
@@ -3,6 +3,6 @@
    define('DB_SERVER', us-cdbr-azure-central-a.cloudapp.net);
    define('DB_USERNAME', jmf1086);
    define('DB_PASSWORD', homefry1);
-   define('DB_DATABASE', TvShows);
+   define('DB_DATABASE', joeytv);
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
