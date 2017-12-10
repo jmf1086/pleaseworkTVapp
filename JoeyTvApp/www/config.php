db3401cbff62adaf7917c2ec6d23874cf1fc4135
jmf1086 <jmf1086@yahoo.com>
2017-12-10 14:38:02 -0600

    f
    f

--- JoeyTvApp/www/config.php
+++ JoeyTvApp/www/config.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <?php
-   define('DB_SERVER', us-cdbr-azure-central-a.cloudapp.net);
+   define('DB_SERVER', joeytvserver.database.windows.net);
    define('DB_USERNAME', jmf1086);
    define('DB_PASSWORD', Homefry1);
    define('DB_DATABASE', joeytv);
    
 	
-   $GLOBALS['db'] = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-	print_r($GLOBALS);
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	
 ?>
